5c2ae968cc10224dcdaa27bc313a6373cf019472
Andres Ariza <afelipe2599@gmail.com>
2020-06-08 23:37:12 -0500

    Pagina inicio de sesion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'semantic-ui-react';
+import { Header, Image, Segment, Grid, Form, Button, Divider, Container } from 'semantic-ui-react';
+import universidad from './assets/universidad.png';
+import upc from './assets/upc.png';
 
 function App() {
   return (
     <div >
-      <Button> Boton semantic </Button>
+      <Header as='h3' block inverted >
+        <Image src={universidad} size='massive'/>
+        Universidad Piloto de Colombia- Prácticas profesionales
+      </Header>
+      <Container>
+        <Segment placeholder>
+          <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column >
+              <Image src={upc} size='medium' centered/>
+            </Grid.Column>
+
+            <Grid.Column verticalAlign='middle'>
+              <Form>
+                <Form.Input
+                  icon='user'
+                  iconPosition='left'
+                  label='Usuario'
+                  placeholder='Usuario'
+                />
+                <Form.Input
+                  icon='lock'
+                  iconPosition='left'
+                  label='Contraseña'
+                  type='Contraseña'
+                />
+
+                <Button primary>Iniciar sesión</Button>
+              </Form>
+            </Grid.Column>
+          </Grid>
+
+          <Divider vertical />
+        </Segment>
+      </Container>
     </div>
   );
 }
